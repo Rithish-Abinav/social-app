43feb5cff8352d3dae974242deeffd5ec7287a1d
Rithishabinav2001 <rithishabinav2001@gmail.com>
2025-04-19 20:45:17 +0530

    Update MongoDB connection to remove deprecated options

--- src/utils/connectMongo.js
+++ src/utils/connectMongo.js
@@ -6,10 +6,7 @@ const connectMongo = async () => {
   if (isConnected) return;
 
   try {
-    const db = await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const db = await mongoose.connect(process.env.MONGO_URL);
 
     isConnected = db.connections[0].readyState;
     console.log("✅ MongoDB Connected");
